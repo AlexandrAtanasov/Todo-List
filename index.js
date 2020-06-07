4359325b4e23c649c9daf5ff925f5cd7ed485569
AtanasovAlexandr <mephistosaa@gmail.com>
2020-06-07 16:43:02 +0300

    some changes

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const Handlebars = require('handlebars')
 const expressHandlebars = require('express-handlebars');
 const todoRoutes = require('./routes/todos')
 
-///
-///
-
 
 const PORT = process.env.PORT || 3000
 
@@ -22,10 +19,6 @@ app.engine('handlebars', expressHandlebars({
 }));
 app.set('view engine', 'handlebars');
 
-
-
-// app.engine('hbs', hbs.engine)
-// app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.urlencoded({ extended: true }))
